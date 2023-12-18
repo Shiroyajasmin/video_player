3568512d00d9f574a3b1d8a9a9354b2f5d8371a0
Your Name <you@example.com>
2023-12-18 18:16:52 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flick_video_player/flick_video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final FlickManager flickManager = FlickManager(
+    videoPlayerController: VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade700,
+        title: const Text("Flutter Video Player"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: AspectRatio(
+          aspectRatio: 16 / 9,
+          child: FlickVideoPlayer(flickManager: flickManager),
+        ),
+      ),
+    );
+  }
+}
